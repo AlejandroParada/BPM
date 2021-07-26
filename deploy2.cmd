725899114e5627293baee08ad4020a3091ce6cc0
Alejandro Parada <alejandaroptaib@gmail.com>
2021-07-26 13:09:42 -0300

    deploy

--- deploy2.cmd
+++ deploy2.cmd
@@ -4,14 +4,15 @@ rem git commit -m "deploy"
 rem git push
 
 
-
-xcopy /S /Y src\.vuepress\dist D:\temp\site
+echo paso 1
+xcopy /S /Y D:\ale\codigo\BPM\src\.vuepress\dist D:\temp\site
 cd D:\ale\codigo\BPM
 rem git pull
 git checkout gh-pages
 git pull
+echo paso 2
 xcopy /S /Y D:\temp\site D:\ale\codigo\BPM
- 
+echo paso 3
 git add .
 git commit -m "deploy"
 git push
