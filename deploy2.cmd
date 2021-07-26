2ac4160f384a5d190d9c4fae56fac8ad309307c5
Alejandro Parada <alejandaroptaib@gmail.com>
2021-07-26 13:21:53 -0300

    deploy

--- deploy2.cmd
+++ deploy2.cmd
@@ -5,12 +5,14 @@ rem git push
 
 
 echo paso 1
+rm -R D:\temp\site
 xcopy /S /Y D:\ale\codigo\BPM\src\.vuepress\dist D:\temp\site
 cd D:\ale\codigo\BPM
 rem git pull
 git checkout gh-pages
 git pull
 echo paso 2
+pause
 xcopy /S /Y D:\temp\site D:\ale\codigo\BPM
 echo paso 3
 git add .
